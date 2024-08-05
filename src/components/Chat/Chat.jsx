301cf53e4e6fbe2d3810de7673c8f7948e488f66
shahryar waseem <you@example.com>
2024-08-06 03:03:35 +0500

    routes add

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -15,7 +15,8 @@ const Chat = () => {
 
   return (
     <df-messenger
-      chat-icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2ArJ-M_QGI0CAcxG9M8N4WUO9kFY5iRI7sQ&amp;s" // Custom chat icon URL
+      chat-icon="https:&#x2F;&#x2F;protoinfrastack-myfirstbucketb8884501-fnnzvxt2ee5v.s3.amazonaws.com&#x2F;EqX7WYx2MmX9CxZtsl8TKNG9u4GMOVh2T1Oy.png"
+      // chat-icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2ArJ-M_QGI0CAcxG9M8N4WUO9kFY5iRI7sQ&amp;s" // Custom chat icon URL
       intent="WELCOME"
       chat-title="🌍 Travel_Bot 🧳"
       agent-id="b8e0895d-9623-4d82-a783-99a7c9bb40d1"
