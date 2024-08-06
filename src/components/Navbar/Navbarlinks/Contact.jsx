0528a14eb7dab07a47a6c9a564157c89ff0d8936
shahryar waseem <you@example.com>
2024-08-06 21:06:18 +0500

    error solved

--- src/components/Navbar/Navbarlinks/Contact.jsx
+++ src/components/Navbar/Navbarlinks/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './contact.css';
+import WOW from 'wowjs';
+import 'animate.css/animate.css';
+import 'wowjs/css/libs/animate.css';
 // import Swal from 'sweetalert2';
 
 const Contact = () => {
@@ -55,17 +58,17 @@ const Contact = () => {
     return (
         <section className="contact">
             <div className="contact-container">
-                <h2 className="contact-title">Contact Us</h2>
+                <h2 className="contact-title wow animate__animated animate__fadeInDown" data-wow-duration="3s" data-wow-delay="1s">Contact Us</h2>
                 <form action="https://formsubmit.co/shahryarwaseem19@gmail.com" className="contact-form" method="POST">
-                    <div className="input-group">
+                    <div className="input-group wow animate__animated animate__heartBeat" data-wow-duration="3s" data-wow-delay="1s">
                         <label htmlFor="name">Full Name</label>
                         <input type="text" id="name" name="name" placeholder="Enter your Name" required />
                     </div>
-                    <div className="input-group">
+                    <div className="input-group wow animate__animated animate__heartBeat" data-wow-duration="3s" data-wow-delay="1s">
                         <label htmlFor="email">Email Address</label>
                         <input type="email" id="email" name="email" placeholder="Enter your Email" required />
                     </div>
-                    <div className="input-group">
+                    <div className="input-group wow animate__animated animate__heartBeat" data-wow-duration="3s" data-wow-delay="1s">
                         <label htmlFor="message">Your Message</label>
                         <textarea id="message" name="message" placeholder="Enter your message" required></textarea>
                     </div>
