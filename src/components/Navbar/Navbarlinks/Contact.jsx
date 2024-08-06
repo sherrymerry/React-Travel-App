ced34372a183e6aeadd015953b65f32189c31781
shahryar waseem <you@example.com>
2024-08-06 21:08:21 +0500

    contact and about page animation added

--- src/components/Navbar/Navbarlinks/Contact.jsx
+++ src/components/Navbar/Navbarlinks/Contact.jsx
@@ -3,57 +3,8 @@ import './contact.css';
 import WOW from 'wowjs';
 import 'animate.css/animate.css';
 import 'wowjs/css/libs/animate.css';
-// import Swal from 'sweetalert2';
 
 const Contact = () => {
-
-    // const onSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const formData = new FormData(event.target);
-
-    //     formData.append("access_key", "9598f476-b2ad-4c35-a4a2-3f97f87eb300");
-
-    //     const object = Object.fromEntries(formData);
-    //     const json = JSON.stringify(object);
-
-    //     try {
-    //         const res = await fetch("https://api.web3forms.com/submit", {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 Accept: "application/json"
-    //             },
-    //             body: json
-    //         }).then((res) => res.json());
-
-    //         console.log(res); 
-
-    //         if (res.success) {
-    //             Swal.fire({
-    //                 title: "Success!",
-    //                 text: "Message sent successfully!",
-    //                 icon: "success",
-    //                 confirmButtonText: "Okay"
-    //             });
-    //         } else {
-    //             Swal.fire({
-    //                 title: "Oops!",
-    //                 text: "Something went wrong. Please try again.",
-    //                 icon: "error",
-    //                 confirmButtonText: "Okay"
-    //             });
-    //         }
-    //     } catch (error) {
-    //         console.error("Error submitting form:", error);
-    //         Swal.fire({
-    //             title: "Error!",
-    //             text: "Network error. Please try again later.",
-    //             icon: "error",
-    //             confirmButtonText: "Okay"
-    //         });
-    //     }
-    // };
-
 
     return (
         <section className="contact">
